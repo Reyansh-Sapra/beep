335fd3b084f7ee10170f2f4cc9bc4ae67021e058
Reyansh Sapra <165906270+Reyansh-Sapra@users.noreply.github.com>
2024-04-11 09:37:02 +0100

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import threading
 import winsound
-import time
 
 app = Flask(__name__)
 
@@ -14,24 +13,20 @@ def beep_thread():
     while not stop_event.is_set():
         if is_beeping:
             winsound.Beep(1000, 100)  # You can adjust the frequency and duration of the beep here
-            time.sleep(interval)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global interval, is_beeping
     if request.method == 'POST':
-        interval = float(request.form['interval'])
-        is_beeping = True
-        stop_event.clear()
-        threading.Thread(target=beep_thread).start()
-    return render_template('index.html', interval=interval)
-
-@app.route('/stop', methods=['POST'])
-def stop():
-    global is_beeping
-    is_beeping = False
-    stop_event.set()
-    return '', 204
+        if 'start' in request.form:
+            interval = float(request.form['interval'])
+            is_beeping = True
+            stop_event.clear()
+            threading.Thread(target=beep_thread).start()
+        elif 'stop' in request.form:
+            is_beeping = False
+            stop_event.set()
+    return render_template('index.html', interval=interval, is_beeping=is_beeping)
 
 if __name__ == '__main__':
     app.run(debug=True)
